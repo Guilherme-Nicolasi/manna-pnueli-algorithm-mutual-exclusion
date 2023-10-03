bd4c07dbbd3a07a9a9c155fc9b10fa90c698e438
Guilherme Nicolasi <guilherme_nicolasi@hotmail.com.br>
2023-10-03 15:44:07 -0300

    code-without-mutual-exclusion

---
+++ main.c
@@ -0,0 +1,58 @@
+#include "pnueli.h"
+
+int main(int argc, char **argv) {
+    pthread_t clients[NUM_THREADS], server;
+    if(pthread_create(&server, NULL, Server, NULL) != 0) {
+        printf("Error: Wasn't possible create server thread.\n");
+        return -1;
+    }
+
+    long i;
+    for(i = 0; i < NUM_THREADS; i++) {
+        if(pthread_create(&clients[i], NULL, Client, (void*)i) != 0) {
+            printf("Error: Wasn't possible create client thread.\n");
+            return -1;
+        }
+    }
+
+    for(i = 0; i < NUM_THREADS; i++) {
+        pthread_join(clients[i], NULL);
+    }
+
+    pthread_join(server, NULL);
+
+    return 0;
+}
+
+void CriticalSection() {
+    int local = soma;
+    sleep((rand() % 2) / 1000);
+    soma = local + 1;
+}
+
+void *Client(void *arg) {
+    long i = (long)arg;
+    while(true) {
+        //non-critical section
+        while(respond != i) {
+            request = i;
+        }
+        //critical section
+        if(soma > MAX_SUM) break;
+        printf("Sum: %ld | Thread: %ld\n", soma, i);
+        CriticalSection();
+
+        respond = 0;
+    }
+    pthread_exit(NULL);
+}
+
+void *Server(void *arg) {
+    while(true) {
+        while(request == 0);
+        respond = request;
+        while(respond != 0);
+        request = 0;
+    }
+    pthread_exit(NULL);
+}
---
+++ pnueli.h
@@ -0,0 +1,20 @@
+#ifndef PNUELI_H_INCLUDED
+#define PNUELI_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define MAX_SUM 100000
+#define NUM_THREADS 4
+
+long soma = 0;
+int request = 0, respond = 0;
+
+void CriticalSection();
+void *Client(void *arg);
+void *Server(void *arg);
+
+#endif // PNUELI_H_INCLUDED
